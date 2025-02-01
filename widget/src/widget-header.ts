69f07dd94ed838063002e652f928e599f42d0e1d
Allan Carvalho <allan.carvalho@dynatrace.com>
2025-02-01 20:51:03 +0100

    feat: added widget header component

---
+++ widget/src/widget-header.ts
@@ -0,0 +1,10 @@
+const widgetHeader = document.createElement('header');
+
+widgetHeader.innerHTML = `
+<header class="widget__header">
+  <p><strong>Págalo en</strong></p>
+  <button id="more-info" class="widget__button">Más info</button>
+</header>
+`;
+
+export default widgetHeader;
--- widget/src/widget.ts
+++ widget/src/widget.ts
@@ -1,15 +1,12 @@
 import './style.css';
 
+import widgetHeader from './widget-header';
+
 export function createWidget(container: Element) {
   const widget = document.createElement('div');
   widget.classList.add('widget__container');
 
-  widget.innerHTML = `
-    <header class="widget__header">
-      <p><strong>Págalo en</strong></p>
-      <button id="more-info" class="widget__button">Más info</button>
-    </header>
-  `;
+  widget.appendChild(widgetHeader);
 
   container.appendChild(widget);
 }
