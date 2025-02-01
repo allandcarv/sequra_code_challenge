eb3e8c610f19e62b61e58e2bdbedd76d742db464
Allan Carvalho <allan.carvalho@dynatrace.com>
2025-02-01 20:44:24 +0100

    feat: added widget header

--- widget/src/widget.ts
+++ widget/src/widget.ts
@@ -1,11 +1,15 @@
 import './style.css';
 
 export function createWidget(container: Element) {
-  const header = document.createElement('h1');
-  header.innerHTML = 'Hello World';
-
   const widget = document.createElement('div');
-  widget.appendChild(header);
+  widget.classList.add('widget__container');
+
+  widget.innerHTML = `
+    <header class="widget__header">
+      <p><strong>Págalo en</strong></p>
+      <button id="more-info" class="widget__button">Más info</button>
+    </header>
+  `;
 
   container.appendChild(widget);
 }
